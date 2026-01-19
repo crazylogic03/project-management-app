74dab2dd17d8188df6d204ab29ec8048c75ade28
crazylogic03 <saithrishul2006@gmail.com>
2026-01-20 00:16:18 +0530

    Updated the scroll feature in chat section

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://project-management-app-89n4.onrender.com/',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/socket.io': {
+        target: 'https://project-management-app-89n4.onrender.com/',
+        ws: true,
+      },
+    },
+  },
 })
